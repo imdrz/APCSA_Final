b79fad68c9275b5e50732593bef4c0254a060c92
BuildTools <unconfigured@null.spigotmc.org>
2025-05-30 12:02:42 -0400

    game update - collision

--- Game.java
+++ Game.java
@@ -7,22 +7,23 @@ public class Game extends JPanel implements KeyListener, ActionListener {
     private int WIDTH = 600, HEIGHT = 600;
     private PlayerObject player1, player2;
     private Rectangle[] platforms;
+    private int xReach = 85, yReach = 85;
 
     public Game() {
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        // this.setBackground(new Color(3, 251, 255));
+        // this.setBackground(new Color(162, 252, 249));
         this.setBackground(Color.white);
         this.setFocusable(true);
         this.addKeyListener(this);
         this.setSize(WIDTH, HEIGHT);
 
-        timer = new Timer(15, this);
-        timer.start();
-
-
-        platforms = new Rectangle[]{new Rectangle(100, 500, 400, 50)};
+        Rectangle bottomBound = new Rectangle(-200, 800, 1200, 30);
+        platforms = new Rectangle[]{new Rectangle(200, 400, 200, 30), new Rectangle(90, 220, 60, 30), new Rectangle(450, 220, 60, 30), new Rectangle(100, 520, 400, 30), bottomBound};
 
         startGame();
+
+        timer = new Timer(15, this);
+        timer.start();
     }
 
     public void startGame() {
@@ -33,8 +34,8 @@ public class Game extends JPanel implements KeyListener, ActionListener {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         
+        g.setColor(Color.gray);
         for (Rectangle p: platforms) {
-            g.setColor(Color.gray);
             g.fillRect(p.x, p.y, p.width, p.height);
         }
 
@@ -42,8 +43,44 @@ public class Game extends JPanel implements KeyListener, ActionListener {
         player2.draw(g);
     }
 
+    public void attack(KeyEvent e) {
+        int[] p1D = player1.getData();
+        int[] p2D = player2.getData();
+        int[] hitCoords;
+        int xD, yD;
+        if (e.getKeyCode() == KeyEvent.VK_S) {
+            hitCoords = player1.getHitCoords();
+            xD = p2D[0] - p1D[0];
+            yD = p2D[1] - p1D[1];
+            if (hitCoords[0] > 0) {
+                if ((xD < xReach * hitCoords[0] && (xD > -15)) && yD <= yReach * hitCoords[1]) {
+                    player2.takeHit(1);
+                }
+            } else {
+                if ((xD > xReach * hitCoords[0] && (xD < 15)) && yD <= yReach * hitCoords[1]) {
+                    player2.takeHit(-1);
+                }
+            }
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            hitCoords = player2.getHitCoords();
+            xD = p1D[0] - p2D[0];
+            yD = p1D[1] - p2D[1];
+            if (hitCoords[0] > 0) {
+                if ((xD < xReach * hitCoords[0] && (xD > -15)) && yD <= yReach * hitCoords[1]) {
+                    player1.takeHit(1);
+                }
+            } else {
+                if ((xD > xReach * hitCoords[0] && (xD < 15)) && yD <= yReach * hitCoords[1]) {
+                    player1.takeHit(-1);
+                }
+            }
+        }
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
+        player1.actionPerformed(e);
+        player2.actionPerformed(e);
         player1.update();
         player2.update();
         repaint();
@@ -63,15 +100,15 @@ public class Game extends JPanel implements KeyListener, ActionListener {
     public void keyReleased(KeyEvent e) {
         player1.keyReleased(e);
         player2.keyReleased(e);
+        attack(e);
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("a");
+        JFrame frame = new JFrame("GameFrame");
         Game game = new Game();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setContentPane(game);
         frame.pack();
         frame.setVisible(true);
     }
-    
 }
--- PlayerObject.java
+++ PlayerObject.java
@@ -1,16 +1,18 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
 public class PlayerObject {
-    private int x, y, dx = 0, dy = 0;
-    private boolean onGround = false;
-    private int velocityP = 0;
-    private int WIDTH = 40, HEIGHT = 60;
+    private int x, y, dx = 0, dy = 0, SPAWN_X, SPAWN_Y;
+    private boolean onGround = false, hasDoubleJump = true;
+    private int WIDTH = 40, HEIGHT = 60, velocityP = 0, lives = 3;
     private Rectangle[] platforms;
     private Color c;
-    private int[] keyBinds;
+    private int[] keyBinds, strokes;
+    private String facing;
+    private double knockback = 0.75;
     
     public PlayerObject(Color c, int x, int y, Rectangle[] platforms, int[] keyBinds) {
         this.x = x;
@@ -18,6 +20,9 @@ public class PlayerObject {
         this.platforms = platforms;
         this.c = c;
         this.keyBinds = keyBinds;
+        this.strokes = new int[]{0, 0, 0};
+        this.SPAWN_X = x;
+        this.SPAWN_Y = y;
     }
 
     public void draw(Graphics g) {
@@ -26,20 +31,17 @@ public class PlayerObject {
     }
 
     public void update() {
-        dx = 5 * velocityP;
+        dx = 6 * velocityP;
         dy++;
 
-        if (dy < -10) {
-            dy = -10;
+        if (dy > 17) {
+            dy = 17;
         }
 
         x += dx;
         y += dy;
 
-
         checkCollision();
-
-        // System.out.println(dx + ", " + dy + "(" + x + ", " + y + ")");
     }
 
     
@@ -50,38 +52,91 @@ public class PlayerObject {
 
     public void checkCollision() {
         for (Rectangle p: platforms) {
-            if (getBounds().intersects(p) && dy >= 0) {
-                y = p.y - HEIGHT;
-                onGround = true;
-                dy = 0;
-                break;
+            if (p.y == platforms[platforms.length-1].y) {
+                if (getBounds().intersects(p)) {
+                    die();
+                }
             } else {
-                onGround = false;
+                if (new Rectangle(x, y+HEIGHT-1, WIDTH, 1).intersects(p)) {
+                    // bottom bound
+                    y = p.y - HEIGHT;
+                    onGround = true;
+                    hasDoubleJump = true;
+                    dy = 0;
+                } else if (new Rectangle(x, y-1, WIDTH, 1).intersects(p)) {
+                    // top bound
+                    y = p.y+p.height;
+                    dy = 0;
+                } else if (new Rectangle(x-1, y, 1, HEIGHT).intersects(p)) {
+                    // left bound
+                    x = p.x+p.width;
+                    dx = 0;
+                } else if (new Rectangle(x+WIDTH-1, y, 1, HEIGHT).intersects(p)) {
+                    // right bound
+                    x = p.x-WIDTH;
+                    dx = 0;
+                }
             }
         }
     }
-
+    
     public int[] getData() {
-        // 0 - 1 - 2 - 3
-        // x - y - w - h
         return new int[]{x, y, WIDTH, HEIGHT};
+    }
+
+    public int[] getHitCoords() {
+        return new int[]{(facing == "right" ? 1 : -1), (dy < 0 ? 1 : 0)};
+    }
+
+    public void die() {
+        x = SPAWN_X;
+        y = SPAWN_Y;
+        lives--;
+        knockback = 0.75;
+    }
+
+    public void takeHit(int dir) {
+        x+= ((int) 45 * knockback * dir);
+        y-= ((int) 20 * knockback);
+        knockback += 0.45;
     }
 
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == keyBinds[1]) {
-            velocityP = 1;
+            strokes[1] = 1;
         } else if (e.getKeyCode() == keyBinds[0]) {
-            velocityP = -1;
+            strokes[0] = 1;
         } else if (e.getKeyCode() == keyBinds[2] && onGround == true) {
-            dy = -55;
+            strokes[2] = 1;
+        } else if (e.getKeyCode() == keyBinds[2] && onGround == false && hasDoubleJump == true) {
+            strokes[2] = 1;
+            hasDoubleJump = false;
         }
     }
 
     public void keyReleased(KeyEvent e) {
         if (e.getKeyCode() == keyBinds[1]) {
-            velocityP = 0;
+            strokes[1] = 0;
         } else if (e.getKeyCode() == keyBinds[0]) {
+            strokes[0] = 0;
+        }
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (strokes[1] == 1) {
+            velocityP = 1;
+            facing = "right";
+        } else if (strokes[0] == 1) {
+            velocityP = -1;
+            facing = "left";
+        } else {
             velocityP = 0;
+        }
+
+        if (strokes[2] == 1 && (onGround == true || hasDoubleJump == false)) {
+            dy = -15;
+            strokes[2] = 0;
+            onGround = false;
         }
     }
 }
